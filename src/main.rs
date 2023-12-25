6820404f773e4c7ac94762877485342e05911943
Ryan DV <rdevilla@uwaterloo.ca>
2023-12-25 13:54:10 -0500

    implement Arbitrary for Graph<usize> for quickcheck testing

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,27 @@ use std::io;
 use std::io::prelude::*;
 use std::collections::HashSet;
 
-struct Graph<T: Eq> {
+#[derive(Clone)]
+struct Graph<T: Clone + Eq + std::hash::Hash> {
     pub vertices: HashSet<T>,
     pub edges: HashSet<(T, T)>,
+}
+
+impl<T: Clone + Eq + std::hash::Hash> Graph<T> {
+    fn from_adjacency_list<I: std::iter::IntoIterator<Item = (T, T)>>(adjacency_list: I) -> Graph<T> {
+        let mut g = Graph {
+            vertices: HashSet::new(),
+            edges: HashSet::new(),
+        };
+
+        adjacency_list.into_iter().for_each(|(u, v)| {
+            g.vertices.insert(u.clone());
+            g.vertices.insert(v.clone());
+            g.edges.insert((u, v));
+        });
+
+        g
+    }
 }
 
 fn main() -> io::Result<()> {
@@ -35,6 +53,23 @@ fn parse_input(input: &str) -> Result<Graph<&str>, &str> {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    // test on graphs with 20 vertices
+    impl quickcheck::Arbitrary for Graph<usize> {
+        fn arbitrary(gen: &mut quickcheck::Gen) -> Graph<usize> {
+            Graph::from_adjacency_list((0..20)
+                .map(|i| (i, (0..20).map(|_| bool::arbitrary(gen)).collect::<Vec<bool>>()))
+                .flat_map(|(i, neighbours)| {
+                    neighbours
+                        .iter()
+                        .enumerate()
+                        .filter(|adjacency| *adjacency.1)
+                        .map(|adjacency| (i, adjacency.0))
+                        .collect::<Vec<(usize, usize)>>()
+                })
+                .collect::<Vec<(usize, usize)>>())
+        }
+    }
 
     #[test]
     fn parses_example_into_graph() {
@@ -97,5 +132,54 @@ mod test {
         let g = parse_input(input).unwrap();
         assert_eq!(expected_vertices, g.vertices);
         assert_eq!(expected_edges, g.edges);
+    }
+
+    #[test]
+    fn graphs_constructible_from_adjacency_lists() {
+        let adjacency_list = HashSet::<(&str, &str)>::from_iter([
+            ("jqt", "rhn"),
+            ("jqt", "xhk"),
+            ("jqt", "nvd"),
+            ("rsh", "frs"),
+            ("rsh", "pzl"),
+            ("rsh", "lsr"),
+            ("xhk", "hfx"),
+            ("cmg", "qnr"),
+            ("cmg", "nvd"),
+            ("cmg", "lhk"),
+            ("cmg", "bvb"),
+            ("rhn", "xhk"),
+            ("rhn", "bvb"),
+            ("rhn", "hfx"),
+            ("bvb", "xhk"),
+            ("bvb", "hfx"),
+            ("pzl", "lsr"),
+            ("pzl", "hfx"),
+            ("pzl", "nvd"),
+            ("qnr", "nvd"),
+            ("ntq", "jqt"),
+            ("ntq", "hfx"),
+            ("ntq", "bvb"),
+            ("ntq", "xhk"),
+            ("nvd", "lhk"),
+            ("lsr", "lhk"),
+            ("rzs", "qnr"),
+            ("rzs", "cmg"),
+            ("rzs", "lsr"),
+            ("rzs", "rsh"),
+            ("frs", "qnr"),
+            ("frs", "lhk"),
+            ("frs", "lsr"),
+        ]);
+        let expected_vertices = HashSet::from_iter([
+            "bvb","cmg","frs","hfx",
+            "jqt","lhk","lsr","ntq",
+            "nvd","pzl","qnr","rhn",
+            "rsh","rzs","xhk"
+        ]);
+
+        let g = Graph::from_adjacency_list(adjacency_list.clone());
+        assert_eq!(expected_vertices, g.vertices);
+        assert_eq!(adjacency_list, g.edges);
     }
 }
